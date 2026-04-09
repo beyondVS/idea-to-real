274ce952d4a47ad731d689fa74cb7d4da40a14a8
callorange <sjy20000@gmail.com>
2026-04-09 11:37:06 +0900

    feat(ui): Create basic chat interface with Django templates and views

--- chat/tests.py
+++ chat/tests.py
@@ -1,8 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 from django.utils import timezone
 from .models import Session, Message, ProblemSpecification
 
 class ModelTest(TestCase):
+    # ... (기존 모델 테스트 코드와 동일) ...
     def setUp(self):
         self.session = Session.objects.create(title="Test Session")
         self.problem_spec = ProblemSpecification.objects.create(
@@ -32,3 +34,19 @@ class ModelTest(TestCase):
         self.assertEqual(self.problem_spec.session, self.session)
         self.assertEqual(self.problem_spec.content["problem"], "Test Problem")
         self.assertIsNotNone(self.problem_spec.version)
+
+class ViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.session = Session.objects.create(title="Test View Session")
+
+    def test_index_view(self):
+        """인덱스 페이지가 정상적으로 응답하는지 확인합니다."""
+        response = self.client.get(reverse('chat:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_detail_view(self):
+        """채팅 상세 페이지가 정상적으로 응답하는지 확인합니다."""
+        response = self.client.get(reverse('chat:detail', args=[self.session.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Test View Session")
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Session, Message
 
-# Create your views here.
+def index(request):
+    """세션 목록 및 새 세션 생성 페이지"""
+    sessions = Session.objects.all()
+    return render(request, 'chat/index.html', {'sessions': sessions})
+
+def detail(request, session_id):
+    """채팅 인터페이스 페이지"""
+    session = get_object_or_404(Session, id=session_id)
+    messages = session.messages.all()
+    return render(request, 'chat/detail.html', {'session': session, 'messages': messages})
+
+def create_session(request):
+    """새 채팅 세션 생성"""
+    if request.method == 'POST':
+        title = request.POST.get('title', 'New Session')
+        session = Session.objects.create(title=title)
+        return redirect('chat:detail', session_id=session.id)
+    return redirect('chat:index')
